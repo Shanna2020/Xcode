324551de5220b55be8a6e3748982b2abed3b55da
Shanna2020 <71110680+Shanna2020@users.noreply.github.com>
2021-10-01 21:08:19 -0400

    Add files via upload

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by user199511 on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var calculatorclass = CalculatorClass()
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBOutlet weak var historyLabel: UILabel!
+    
+ //initalization of variable///////////////////
+    
+   
+    var displayString:String=""
+    var historyString=""
+    var mode = false
+    var errVar = 9999999
+
+    
+    @IBAction func click_handler(_ sender: Any) {
+      
+    //if the standard/advance button is clicked
+        if (sender as! UIButton).tag == 16{ //mode button
+            mode=calculatorclass.mode()// call calculator mode function
+            if mode==true
+            { //change label if advance is selected
+              (sender as! UIButton).setTitle("Advance - History ", for: .normal)
+                historyLabel.isHidden=false
+                
+            }else{ //Change label if standard is selected
+          
+                (sender as! UIButton).setTitle("Standard - No History", for: .normal)
+                historyLabel.isHidden=true
+
+            } //end else
+        } //end tag 16 if statement
+        else{
+            
+        if (sender as! UIButton).tag == 11//equal sign selected
+        {
+            let finalCal:Int = calculatorclass.calc()// call to calc function
+            
+            if finalCal == errVar // error number to check
+            {
+                displayString = ""
+                displayString += "Error"
+                displayLabel.text = displayString
+                clearFunction()
+              
+            }//end if
+            else{ //add equal sign to display string and add string to label
+                displayString += "=" + String(finalCal)
+                displayLabel.text = displayString
+            
+                if mode==true{ //if mode is true add display string to history string
+                historyString += " \(displayString)"
+                
+                historyLabel.text = historyString
+                
+                    } // end if mode
+                clearFunction() //clear strings
+            
+            }// end else
+        }else if(sender as! UIButton).tag == 9 //clear button
+            {
+          clearFunction()
+          displayLabel.text = ""
+            }
+        else{
+            // call calculator push function to add text
+            calculatorclass.push(data: (sender as! UIButton).titleLabel!.text ?? "Error")
+                   
+            //add to string and label
+            displayString += ((sender as! UIButton).titleLabel!.text ?? "Error")
+            displayLabel.text = displayString
+            
+        }//end else
+    }//end out if
+        
+    }//end click_handle function
+    
+    ///////////////////////ClearFunction///////////////
+    func clearFunction(){
+        displayString=""
+        calculatorclass.pop() //clear array
+    }
+    
+ 
+    ///////////////////////////////////////////////////////
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }// end viewDidLoan
+
+
+}
+
---
+++ Calculator/CalculatorClass.swift
@@ -0,0 +1,147 @@
+//
+//  CalculatorClass.swift
+//  Calculator
+//
+//  Created by user199511 on 9/29/21.
+//
+
+import Foundation
+import UIKit
+
+class CalculatorClass{
+    
+    var arrayString:[String]=[]
+    //var historyArray:[String]=[]
+    //var displayString:String=""
+    var errorVar = 9999999
+  //  var calNum=0, secNum=0;
+    var modeVar = false
+    
+    //Push function///////////////////////////////////////////////////////
+    
+    func push(data: String){
+        
+        arrayString.append(data)
+        
+    }//end of push function
+    
+    //pop function ////////////////////////////////////////////////////////
+    
+    func pop()
+    {
+        arrayString.removeAll();
+        
+    }
+   
+    //mode function////////////////////////////////////////////////////////
+    
+    func mode()->BooleanLiteralType
+    {
+        //changes the mode variable each time the button is clicked
+        if modeVar==false
+        {
+            modeVar=true
+                     
+        }else{
+            modeVar=false
+                   
+        }
+
+        return modeVar
+    }// end of mode
+    
+    
+    
+    
+ /////////////////////Cal c Function////////////////////////////////////
+ func calc()-> Int{
+    var calNum:Int=0 , secNum=0;
+     var i=0 , j=0, fnumtest = 0, snumtest = 0;
+     
+     //Allow testing of number of operand
+     let testNum = ["9":1,"8":1, "7":1 , "6":1, "5":1, "4":1, "3":1, "2":1, "1":1, "0":1 , "+":2, "-":2, "*":2]
+     
+
+     
+if arrayString.isEmpty{ //test if array is empty
+ return errorVar
+   }else{
+     
+while(j < arrayString.count)
+{ //control looping through string to check same operation
+ if fnumtest==0 //testing for first number
+ {
+      for (num, numValue) in testNum //get the key and value from testNum
+     {
+         if(num==arrayString[j])
+         {
+             fnumtest = numValue //assigning tthe value to the first variable
+            if fnumtest==2{return errorVar} //if first number is not a number return error
+          }
+         
+      } //end for
+     
+ } else{ // if it is not the first number
+         for (num, numValue) in testNum
+         {
+             if num==arrayString[j]
+             {
+             snumtest = numValue //assign the value of second variable
+             }
+         } //end for
+         if ((fnumtest == 1 && snumtest==1) || (fnumtest == 2 && snumtest==2)) //test the values of first variable and second variable
+         {
+            return errorVar //return error variable if they are the same
+
+         } else{
+             fnumtest=snumtest //assign value of second number to first number if they are not
+                 }
+     } //close else
+ j+=1
+ 
+} //end while
+   
+}//end else
+     
+  //Calculating numbers//////////////////////////////////////////
+while i < arrayString.count
+ {
+     if calNum == 0
+     {
+        calNum = Int(arrayString[0])!
+           i+=1
+     } // check the first number
+      //do calculation based on operands
+    switch arrayString[i]{
+         case"+":
+             
+             secNum = Int(arrayString[i+1])!
+             calNum+=secNum
+             
+         case"-":
+             
+             secNum = Int(arrayString[i+1])!
+             calNum-=secNum
+         case"*":
+             secNum = Int(arrayString[i+1])!
+            calNum*=secNum
+         case"/":
+             secNum = Int(arrayString[i+1])!
+             calNum=calNum / secNum
+                  
+         default:
+            calNum+=0
+
+
+     }// end switch
+
+     i+=1
+
+ }//end while
+
+    return calNum
+ 
+ } //end of calc function
+
+    
+}// end of calculationclass
